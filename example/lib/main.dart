96240d5d50d08c55ff2bcd2e288d4362737fcc49
Ishita Shukla <ishitashukla120603@gmail.com>
2024-08-01 22:32:51 +0530

    web_update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,42 @@
+// import 'package:flutter/material.dart';
+// import 'package:kipps_chatbot/kipps_chatbot.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       floatingActionButton:
+//           ChatbotIntegration(chatbotId: '868aafc1-74b3-4c44-93ec-8fac3b4b7cf3'),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:kipps_chatbot/kipps_chatbot.dart';
 
@@ -17,6 +56,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -31,9 +71,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton:
-          ChatbotIntegration(chatbotId: '<your chaatbotId>'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('Press the chat button to open the chatbot.'),
+      ),
+      floatingActionButton: const ChatbotIntegration(
+        chatbotId: '868aafc1-74b3-4c44-93ec-8fac3b4b7cf3', // Update with your chatbot ID
+      ),
     );
   }
 }
+
--- lib/src/package_1.dart
+++ lib/src/package_1.dart
@@ -1,8 +1,200 @@
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+// import 'dart:io';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// class ChatbotIntegration extends StatefulWidget {
+//   final String chatbotId;
+
+//   const ChatbotIntegration({
+//     required this.chatbotId,
+//     super.key,
+//   });
+
+//   @override
+//   ChatbotIntegrationState createState() => ChatbotIntegrationState();
+// }
+
+// class ChatbotIntegrationState extends State<ChatbotIntegration> {
+//   late String _chatbotUrl;
+//   String? _chatIconUrl;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _chatbotUrl = 'https://app.kipps.ai/iframe/${widget.chatbotId}';
+//     _fetchChatIcon();
+//     if (Platform.isAndroid) {
+//       WebView.platform = SurfaceAndroidWebView();
+//     }
+//   }
+
+//   Future<void> _fetchChatIcon() async {
+//     final response = await http.get(Uri.parse(
+//         'https://backend.kipps.ai/kipps/chatbot/${widget.chatbotId}'));
+//     if (response.statusCode == 200) {
+//       final data = json.decode(response.body);
+//       setState(() {
+//         _chatIconUrl = data['chat_icon_image'];
+//       });
+//     } else {
+//       throw Exception('Failed to load chatbot icon');
+//     }
+//   }
+
+//   void _showChatbotModal(BuildContext context) {
+//     showModalBottomSheet(
+//       context: context,
+//       isScrollControlled: true,
+//       builder: (BuildContext context) {
+//         return FractionallySizedBox(
+//           heightFactor: 0.9,
+//           child: Stack(
+//             children: [
+//               WebView(
+//                 initialUrl: _chatbotUrl,
+//                 javascriptMode: JavascriptMode.unrestricted,
+//               ),
+//               Positioned(
+//                 top: 10,
+//                 right: 10,
+//                 child: GestureDetector(
+//                   onTap: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                   child: const CircleAvatar(
+//                     radius: 15,
+//                     backgroundColor: Colors.black,
+//                     child: Icon(
+//                       Icons.close,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CircleAvatar(
+//       // radius: 30,
+//       child: FloatingActionButton(
+//           onPressed: () => _showChatbotModal(context),
+//           child: _chatIconUrl != null
+//               ? Image.network(_chatIconUrl!)
+//               : Image.network(
+//                   'https://chatx-ai-dev.s3.amazonaws.com/Primary.png')),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// class ChatbotIntegration extends StatefulWidget {
+//   final String chatbotId;
+
+//   const ChatbotIntegration({
+//     required this.chatbotId,
+//     super.key,
+//   });
+
+//   @override
+//   ChatbotIntegrationState createState() => ChatbotIntegrationState();
+// }
+
+// class ChatbotIntegrationState extends State<ChatbotIntegration> {
+//   late WebUri _chatbotUrl;
+//   String? _chatIconUrl;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _chatbotUrl = WebUri('https://app.kipps.ai/iframe/${widget.chatbotId}');
+//     _fetchChatIcon();
+//   }
+
+//   Future<void> _fetchChatIcon() async {
+//     final response = await http.get(Uri.parse(
+//         'https://backend.kipps.ai/kipps/chatbot/${widget.chatbotId}'));
+//     if (response.statusCode == 200) {
+//       final data = json.decode(response.body);
+//       setState(() {
+//         _chatIconUrl = data['chat_icon_image'];
+//       });
+//     } else {
+//       throw Exception('Failed to load chatbot icon');
+//     }
+//   }
+
+//   void _showChatbotModal(BuildContext context) {
+//     showModalBottomSheet(
+//       context: context,
+//       isScrollControlled: true,
+//       enableDrag: false,
+//       builder: (BuildContext context) {
+//         return FractionallySizedBox(
+//           heightFactor: 0.9,
+//           child: Stack(
+//             children: [
+//               InAppWebView(
+//                 initialUrlRequest: URLRequest(url: _chatbotUrl),
+//                 initialSettings: InAppWebViewSettings(
+//                   javaScriptEnabled: true,
+//                 ),
+//               ),
+//               Positioned(
+//                 top: 10,
+//                 right: 10,
+//                 child: GestureDetector(
+//                   onTap: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                   child: const CircleAvatar(
+//                     radius: 15,
+//                     backgroundColor: Colors.black,
+//                     child: Icon(
+//                       Icons.close,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CircleAvatar(
+//       child: FloatingActionButton(
+//         onPressed: () => _showChatbotModal(context),
+//         child: _chatIconUrl != null
+//             ? Image.network(_chatIconUrl!)
+//             : Image.network(
+//                 'https://chatx-ai-dev.s3.amazonaws.com/Primary.png'),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:io';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ChatbotIntegration extends StatefulWidget {
   final String chatbotId;
@@ -17,17 +209,15 @@ class ChatbotIntegration extends StatefulWidget {
 }
 
 class ChatbotIntegrationState extends State<ChatbotIntegration> {
-  late String _chatbotUrl;
+  late WebUri _chatbotUrl;
   String? _chatIconUrl;
+  OverlayEntry? _overlayEntry;
 
   @override
   void initState() {
     super.initState();
-    _chatbotUrl = 'https://app.kipps.ai/iframe/${widget.chatbotId}';
+    _chatbotUrl = WebUri('https://app.kipps.ai/iframe/${widget.chatbotId}');
     _fetchChatIcon();
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
   }
 
   Future<void> _fetchChatIcon() async {
@@ -43,18 +233,29 @@ class ChatbotIntegrationState extends State<ChatbotIntegration> {
     }
   }
 
+  void _showChatbot(BuildContext context) {
+    if (kIsWeb) {
+      _showChatbotOverlay(context);
+    } else {
+      _showChatbotModal(context);
+    }
+  }
+
   void _showChatbotModal(BuildContext context) {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
+      enableDrag: false,
       builder: (BuildContext context) {
         return FractionallySizedBox(
           heightFactor: 0.9,
           child: Stack(
             children: [
-              WebView(
-                initialUrl: _chatbotUrl,
-                javascriptMode: JavascriptMode.unrestricted,
+              InAppWebView(
+                initialUrlRequest: URLRequest(url: _chatbotUrl),
+                initialSettings: InAppWebViewSettings(
+                  javaScriptEnabled: true,
+                ),
               ),
               Positioned(
                 top: 10,
@@ -80,16 +281,91 @@ class ChatbotIntegrationState extends State<ChatbotIntegration> {
     );
   }
 
+  void _showChatbotOverlay(BuildContext context) {
+  _overlayEntry = OverlayEntry(
+    builder: (context) => Stack(
+      children: [
+        Positioned.fill(
+          child: GestureDetector(
+            onTap: () {
+              _overlayEntry?.remove();
+              _overlayEntry = null;
+              setState(() {});
+            },
+            child: Container(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 70,
+          right: 20,
+          child: Material(
+            elevation: 8.0,
+            color: Colors.transparent,
+            child: Stack(
+              children: [
+                Container(
+                  width: 400,
+                  height: 600,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                      ),
+                    ],
+                  ),
+                  child: InAppWebView(
+                    initialUrlRequest: URLRequest(url: _chatbotUrl),
+                    initialSettings: InAppWebViewSettings(
+                      javaScriptEnabled: true,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: GestureDetector(
+                    onTap: () {
+                      _overlayEntry?.remove();
+                      _overlayEntry = null;
+                      setState(() {});
+                    },
+                    child: const CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Colors.black,
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  Overlay.of(context).insert(_overlayEntry!);
+}
+
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      // radius: 30,
       child: FloatingActionButton(
-          onPressed: () => _showChatbotModal(context),
-          child: _chatIconUrl != null
-              ? Image.network(_chatIconUrl!)
-              : Image.network(
-                  'https://chatx-ai-dev.s3.amazonaws.com/Primary.png')),
+        onPressed: () => _showChatbot(context),
+        child: _chatIconUrl != null
+            ? Image.network(_chatIconUrl!)
+            : Image.network(
+                'https://chatx-ai-dev.s3.amazonaws.com/Primary.png'),
+      ),
     );
   }
 }
